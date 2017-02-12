da08c613152d0f3a7e1e3f7896e981eea4517c9d
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2017-02-12 01:01:03 -0800

    added random mood

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
   * [rust-lang/rust](https://github.com/rust-lang/rust) -- [www.rust-lang.org](https://www.rust-lang.org)
   * [rust-lang/cargo](https://github.com/rust-lang/cargo) -- [crates.io](https://crates.io)
   * [24 days of Rust](https://github.com/zsiciarz/24daysofrust)
+  * [toml](https://github.com/toml-lang/toml)
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
-// use rand::Rnd;
+extern crate rand;
+use rand::Rng;
 
-fn main() {
-    let mood = "curious";
+fn main () {
     // TODO pick random from https://moodlist.net/
+    let moods = [
+        "Accepted", "Accomplished", "Aggravated", "Alone", "Amused", "Angry", "Annoyed", "Anxious",
+        "Apathetic", "Apologetic", "Ashamed", "Awake", "Bewildered", "Bitchy", "Bittersweet",
+        "Blah", "Blank", "Blissful", "Bored"
+    ];
+    let mut rng = rand::thread_rng();
+    let ptr = rng.gen::<usize>() % moods.len();
+    let mood = moods[ptr];
     println!("I am {}", mood);
 }
